1a858518f6bae3025a03d3a16f82412a3f9bc74d
ATOMPassakorn <atom15440@gmail.com>
2026-02-01 20:41:50 +0700

    lab7 (example)

---
+++ lab7/example/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const path = require('path');
+// Serve static files from multiple directories
+app.use(express.static('public'));
+// app.use(express.static('files'));
+// Or specify a virtual path prefix
+// app.use('/static', express.static(path.join(__dirname, 'public')))
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname,'public/home.html'));
+});
+
+app.get('/about', function (req, res) {
+    res.sendFile(path.join(__dirname,'public/about.html'));
+});
+
+app.get('/form', function (req, res) {
+    res.sendFile(path.join(__dirname,'public/form.html'));
+});
+
+// Route handling query parameters
+app.get('/submitform', (req, res) => {
+  // Access query parameters using req.query
+  const { fname, lname } = req.query;
+  res.send(`First name: ${fname}, Last name: ${lname}`);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}, press Ctrl-C to terminate....`)
+})
